a992b2ea1e8bc3553cc748ec33107ad53adac2b7
wolf2234 <viktoral9599@gmail.com>
2025-03-27 00:09:23 +0300

    this commit adds waves.

--- includes/sub-header.php
+++ includes/sub-header.php
@@ -10,9 +10,9 @@
                     </div>
                 </div>
             </div>
-            <div class="pure-u-1 pure-u-sm-1-2" style="border: 1px solid red;">
+            <div class="pure-u-1 pure-u-sm-1-2">
                 <div class="schema">
-                    <div class="schema__img">
+                    <div class="schema__image">
                         <img src="wp-content/themes/api2cart/assets/img/your-app-software.png" alt="Your App Software">
                     </div>
                     <div class="arrows">
@@ -24,13 +24,15 @@
                         </div>
                         <div class="arrow right"></div>
                     </div>
-                    <div class="schema__img">
-                        <div class="words">
-                            <span class="word">Привет</span>
-                            <span class="word">Как дела?</span>
-                            <span class="word">Отлично!</span>
+                    <div class="schema__image">
+                        <div class="schema__img">
+                            <div class="words">
+                                <span class="word">product</span>
+                                <span class="word">category</span>
+                                <span class="word">order</span>
+                            </div>
+                            <img src="wp-content/themes/api2cart/assets/img/api-flower-white.png" alt="API Flower White">
                         </div>
-                        <img src="wp-content/themes/api2cart/assets/img/api-flower-white.png" alt="API Flower White">
                     </div>
                     <div class="arrows">
                         <div class="arrow left"></div>
@@ -41,11 +43,25 @@
                         </div>
                         <div class="arrow right"></div>
                     </div>
-                    <div class="schema__img">
+                    <div class="schema__image">
                         <img src="wp-content/themes/api2cart/assets/img/supported-platforms-scheme.png" alt="Supported Platforms Scheme">
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="wave-container">
+    <svg  viewBox="0 0 800 100" preserveAspectRatio="none">
+        <path d="M0,50 C10,90 20,10 30,50 C40,90 50,10 60,50 C70,90 80,10 90,50 
+                    C100,90 110,10 120,50 C130,90 140,10 150,50 C160,90 170,10 180,50 
+                    C190,90 200,10 210,50 C220,90 230,10 240,50 C250,90 260,10 270,50 
+                    C280,90 290,10 300,50 C310,90 320,10 330,50 C340,90 350,10 360,50 
+                    C370,90 380,10 390,50 C400,90 410,10 420,50 C430,90 440,10 450,50 
+                    C460,90 470,10 480,50 C490,90 500,10 510,50 C520,90 530,10 540,50 
+                    C550,90 560,10 570,50 C580,90 590,10 600,50 C610,90 620,10 630,50
+                    C640,90 650,10 660,50 C670,90 680,10 690,50 C700,90 710,10 720,50
+                    C730,90 740,10 750,50 C760,90 770,10 780,50 L780,100 L0,100 Z"
+                style="stroke: none; fill: white;"></path>
+    </svg>
+</div>
 </section>

21f3c02232b8358e7e3077013b7ff60f1cd45d73
wolf2234 <viktoral9599@gmail.com>
2025-03-26 05:57:39 +0300

    this commit adds button-groups.

--- includes/header.php
+++ includes/header.php
@@ -5,33 +5,152 @@
                 <div class="logo"><?php the_custom_logo($blog_id); ?></div>
             </div>
             <div class="pure-u-1 pure-u-sm-1-6">
-                Search
+                <div class="search">
+                    <div class="search__input">
+                        <input type="text" placeholder="Find something…">
+                        <img src="wp-content/themes/api2cart/assets/img/api2cart_search.png" alt="">
+                    </div>
+                </div>
             </div>
             <div class="pure-u-1 pure-u-sm-1-2">
                 <div class="pure-menu pure-menu-horizontal">
                     <ul class="pure-menu-list">
                         <li class="pure-menu-item">
-                            <a href="#" class="pure-menu-link">How It Works</a>
+                            <div class="menu-item">
+                                <a href="#" class="pure-menu-link">How It Works</a>
+                            </div>
+                            <div class="dropdown">
+                                <div class="dropdown__body">
+                                    <div class="dropdown__items">
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/how-it-works/" class="dropdown__link">How It Works</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/e-commerce-api-integration-benefits-for-business/" class="dropdown__link">Benefits for Business</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/e-commerce-api-integration-benefits-for-development/" class="dropdown__link">Benefits for Developers</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/e-commerce-api-integration-benefits-for-startups/" class="dropdown__link">Benefits for Startups</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/tco-calculator/" class="dropdown__link">TCO calculator</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/webinar-demo-introduction/" class="dropdown__link">Webinar Demo Introduction</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                         <li class="pure-menu-item">
-                            <a href="#" class="pure-menu-link">Pricing</a>
+                            <div class="menu-item">
+                                <a href="#" class="pure-menu-link">Pricing</a>
+                            </div>
                         </li>
                         <li class="pure-menu-item">
-                            <a href="#" class="pure-menu-link">Use Cases</a>
+                            <div class="menu-item">
+                                <a href="#" class="pure-menu-link">Use Cases</a>
+                            </div>
+                            <div class="dropdown">
+                                <div class="dropdown__body">
+                                    <div class="dropdown__items">
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/use-cases/" class="dropdown__link">Industry Cases</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/business-cases/" class="dropdown__link">Business Cases</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/resources/" class="dropdown__link">Case Studies</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                         <li class="pure-menu-item">
-                            <a href="#" class="pure-menu-link">Integrations</a>
+                            <div class="menu-item">
+                                <a href="#" class="pure-menu-link">Integrations</a>
+                            </div>
+                            <div class="dropdown">
+                                <div class="dropdown__body">
+                                    <div class="dropdown__items">
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/supported-platforms/" class="dropdown__link">Supported Platforms</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/planned-platforms/" class="dropdown__link">Planned Platforms</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/supported-api-methods/" class="dropdown__link">Supported Methods</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/webhooks/" class="dropdown__link">API2Cart Webhooks</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                         <li class="pure-menu-item">
-                            <a href="#" class="pure-menu-link">Docs</a>
+                            <div class="menu-item">
+                                <a href="#" class="pure-menu-link">Docs</a>
+                            </div>
+                            <div class="dropdown">
+                                <div class="dropdown__body">
+                                    <div class="dropdown__items">
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/docs/" class="dropdown__link">Documentation</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/docs/sdk/" class="dropdown__link">SDK</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/changelog/" class="dropdown__link">Changelog</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/faqs/" class="dropdown__link">FAQ</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                         <li class="pure-menu-item">
-                            <a href="#" class="pure-menu-link">Company</a>
+                            <div class="menu-item">
+                                <a href="#" class="pure-menu-link">Company</a>
+                            </div>
+                            <div class="dropdown">
+                                <div class="dropdown__body">
+                                    <div class="dropdown__items">
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/about-api2cart/" class="dropdown__link">About Us</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/blog/" class="dropdown__link">Blog</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/testimonials/" class="dropdown__link">Testimonials</a>
+                                        </div>
+                                        <div class="dropdown__item">
+                                            <a href="https://api2cart.com/career/" class="dropdown__link">Career</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                     </ul>
                 </div>
             </div>
-            <div class="pure-u-1 pure-u-sm-1-6"><p>Thirds</p></div>
+            <div class="pure-u-1 pure-u-sm-1-6">
+                <div class="button-groups">
+                    <div class="login">
+                        <a href="https://app.api2cart.com/?demo=true" class="login__btn">Login</a>
+                    </div>
+                    <div class="demo">
+                        <a href="https://api2cart.com/book-a-demo/" class="demo__btn">Book a Demo</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </header>

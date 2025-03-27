d9e9140ca46db1355b9c75239544f0ab1e2ccfdb
wolf2234 <viktoral9599@gmail.com>
2025-03-27 10:46:52 +0300

    this commit adds alt into brand__image->img.

--- includes/trusted.php
+++ includes/trusted.php
@@ -6,42 +6,42 @@
                 <div class="pure-u-1-2 pure-u-md-1-1 pure-u-lg-1-6">
                     <div class="brand">
                         <div class="brand__image">
-                            <img src="wp-content/themes/api2cart/assets/img/saashub-rate.png" alt="">
+                            <img src="wp-content/themes/api2cart/assets/img/saashub-rate.png" alt="Saashub Rate">
                         </div>
                     </div>
                 </div>
                 <div class="pure-u-1-2 pure-u-md-1-1 pure-u-lg-1-6">
                     <div class="brand">
                         <div class="brand__image">
-                            <img src="wp-content/themes/api2cart/assets/img/quality-rate.png" alt="">
+                            <img src="wp-content/themes/api2cart/assets/img/quality-rate.png" alt="Quality Rate">
                         </div>
                     </div>
                 </div>
                 <div class="pure-u-1-2 pure-u-md-1-1 pure-u-lg-1-6">
                     <div class="brand">
                         <div class="brand__image">
-                            <img src="wp-content/themes/api2cart/assets/img/capterra-rate.png" alt="">
+                            <img src="wp-content/themes/api2cart/assets/img/capterra-rate.png" alt="Capterra Rate">
                         </div>
                     </div>
                 </div>
                 <div class="pure-u-1-2 pure-u-md-1-1 pure-u-lg-1-6">
                     <div class="brand">
                         <div class="brand__image">
-                            <img src="wp-content/themes/api2cart/assets/img/happy-rate.png" alt="">
+                            <img src="wp-content/themes/api2cart/assets/img/happy-rate.png" alt="Happy Rate">
                         </div>
                     </div>
                 </div>
                 <div class="pure-u-1-2 pure-u-md-1-1 pure-u-lg-1-6">
                     <div class="brand">
                         <div class="brand__image">
-                            <img src="wp-content/themes/api2cart/assets/img/trustpilot-rate.png" alt="">
+                            <img src="wp-content/themes/api2cart/assets/img/trustpilot-rate.png" alt="Trustpilot Rate">
                         </div>
                     </div>
                 </div>
                 <div class="pure-u-1-2 pure-u-md-1-1 pure-u-lg-1-6">
                     <div class="brand">
                         <div class="brand__image">
-                            <img src="wp-content/themes/api2cart/assets/img/cioreview-rate.png" alt="">
+                            <img src="wp-content/themes/api2cart/assets/img/cioreview-rate.png" alt="Cioreview Rate">
                         </div>
                     </div>
                 </div>
